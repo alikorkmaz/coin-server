f38012db49e21672e7f93cfd7b34880fbcf218a1
alikorkmaz <30934578+alikorkmaz@users.noreply.github.com>
2019-03-17 01:05:33 +0300

    initial

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/koineks", (req, res) => {
+  fetch("https://koineks.com/ticker")
+    .then(response => response.json())
+    .then(json => res.send(json));
+});
+
+app.get("/paribu", (req, res) => {
+  fetch("https://paribu.com/ticker")
+    .then(response => response.json())
+    .then(json => res.send(json));
+});
+
+app.get("/btcturk", (req, res) => {
+  fetch("https://www.btcturk.com/api/ticker")
+    .then(response => response.json())
+    .then(json => res.send(json));
+});
+
+app.listen(3001, () => console.log("listening on 3001"));
