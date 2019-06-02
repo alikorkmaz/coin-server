c04c7c8c6a26e7970b826a77993f8aa111bd6178
Ali Korkmaz <ali@Alis-MacBook-Pro.local>
2019-06-02 12:55:14 +0300

    json-db

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
 
+const { exec } = require("child_process");
+
+// Set port (default: 3000). For Heroku, we need to use
+// the port set by the environment variable $PORT
+const port = process.env.PORT || 3000;
+
+const command = `json-server --watch db.json --port ${port}`;
+
+exec(command, (err, stdout, stderr) => {
+  if (err) {
+    console.log("Error running exec", err);
+    return;
+  }
+  console.log("stdout:", stdout);
+  console.log("stderr:", stderr);
+});
+
 let kur;
 setInterval(() => {
   fetch(
