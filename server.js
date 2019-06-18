d83b4a93b7f6f8e5138552730af18e9baf77c9b1
Ali Korkmaz <ali@Alis-MacBook-Pro.local>
2019-06-19 01:48:14 +0300

    bum

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ var p = new Push({
   token: 'a26qjmrach23epfar8zatfh7apcyfd',
 });
 
-let profitMargin = 0.1;
+let profitMargin = 0.05;
 let text = '';
 setInterval(() => {
   if (kur === 0) return;
