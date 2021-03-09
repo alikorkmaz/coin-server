fea88f3d88c40ecc0fa648c902def9dc90d6b533
alikorkmaz <ali.korkmaz@getmidas.com>
2021-03-09 23:09:31 +0300

    updates cron

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ var p = new Push({
   token: 'a26qjmrach23epfar8zatfh7apcyfd',
 });
 
-let profitMargin = 0.01;
+let profitMargin = 0.05;
 let profitMarginReverse = 0;
 let text = '';
 setInterval(() => {
