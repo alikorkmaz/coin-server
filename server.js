c94b014555b1c4ba684bf02bda8a0872b9dfb878
alikorkmaz <ali.korkmaz@getmidas.com>
2021-03-18 23:03:51 +0300

    adds cash withdrawable

--- server.js
+++ server.js
@@ -150,14 +150,15 @@ app.get('/koineks', (req, res) => {
 });
 
 app.get('/paribu', (req, res) => {
-  fetch('https://8080-fe8ca129-39a3-4154-be62-c24bdfd0c004.europe-west4.cloudshell.dev/paribu')
+  fetch('https://www.paribu.com/ticker')
     .then(response => {
       console.log(response, response.json()); return response.json();
     })
     .then(json => res.send(json))
     .catch((error) => {
-  console.error(error);
-});
+      console.error(error);
+      res.send({error:error});
+    });
 });
 
 app.get('/btcturk', (req, res) => {
