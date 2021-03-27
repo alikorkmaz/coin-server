3702e0b9830b06221ec583190baa099f98d5a23f
alikorkmaz <ali.korkmaz@getmidas.com>
2021-03-27 09:21:33 +0300

    m

--- server.js
+++ server.js
@@ -777,7 +777,7 @@ app.get('/coinbase', async (req, res) => {
     let koineksData = await getKoineksData();
     if (paribu) {
 
-        tetherBuy = +paribu.USDT_TL.lowestAsk + 0.045;
+        tetherBuy = +paribu.USDT_TL.lowestAsk + 0.025;
 
 
         if (paribu.UNI_TL)
