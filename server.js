1a5f39c83cd2d4e9b7b47e05620a500446db13b9
Ali Korkmaz <ali@Alis-MacBook-Pro.local>
2021-06-22 19:35:09 +0300

    ogn

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ var Push = require('pushover-notifications');
 
 var p = new Push({
     user: 'g6qgivbzbg1nrakurqaaecmwrmcaxj',
-    token: 'aimiivzn6eh82mih6n21vu347aneum',
+    token: 'aqoyrmbrtmau2q7jfjobgo6p7sa4om',
 });
 
 let profitMargin = 0.1;
