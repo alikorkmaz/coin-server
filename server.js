b9242894d9d8d4b80c300b7e08b9438ff5959f5e
alikorkmaz <30934578+alikorkmaz@users.noreply.github.com>
2022-05-31 03:28:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -572,7 +572,7 @@ setInterval(async function(){
 
 
         data.filter(pair => (pair.title.includes("BTCTURK") 
-                             && !pair.title.includes("LUuuuuNA") 
+                             && !pair.title.includes("LUNC") 
                              && !pair.title.includes("TERrrrrrrrRA") 
                              && !pair.title.includes("asdfasd") 
                              && !pair.title.includes("ddddddddd") 
