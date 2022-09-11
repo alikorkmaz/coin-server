f65aaf4fd9744fa73ad1230b519342adcbd51a82
alikorkmaz <30934578+alikorkmaz@users.noreply.github.com>
2022-09-11 20:46:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -177,6 +177,36 @@ app.get('/alert-reverse', (req, res) => {
 //547f1508205c1568706666c56bc02f4e
 //dbd68dd34460118330481bafbcc9740d
 
+
+setInterval(() => {
+    fetch('https://v3.paribu.com/app/initials')
+        .then(response => response.json())
+        .then(response => {
+            
+            if (response.data.currencies.rad.withdraw.enabled)
+                ee.send({
+                        message: "rad acildi",
+                    },
+                    function(err, result) {
+                        {};
+                    },
+                );
+
+            if (response.data.currencies.chz.withdraw.enabled)
+                ee.send({
+                        message: "fet acildi",
+                    },
+                    function(err, result) {
+                        {};
+                    },
+                );
+
+        })
+        .catch(x => {
+            console.log(x);
+        });
+}, 5000);
+
 let kur = 8.5;
 setInterval(() => {
     //4a267b6b3eaa5f8fd0a2e9e08852741b
